f61732f7f62c85e08d7e5b679ba1223d58d4ef46
danieelsan1y <danieelsan1y@gmail.com>
2024-01-31 21:51:08 -0300

    inclusão da classe inerval

---
+++ src/main/java/com/insight/controledejornada/model/Interval.java
@@ -0,0 +1,23 @@
+package com.insight.controledejornada.model;
+
+import com.insight.controledejornada.dto.WorkTimeDTO;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalTime;
+
+import static com.insight.controledejornada.utils.HourUtils.getLocalTime;
+
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+public class Interval {
+
+    private final LocalTime input;
+
+    private final LocalTime output;
+
+}
